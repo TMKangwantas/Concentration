0ba32df928e12c007299a020c6529d284c0587ba
TMKangwantas <tmkangwantas@gmail.com>
2019-01-16 19:08:13 -0500

    Added flipCount label and variable to keep track of amount of times card has been flipped.

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Mark Kangwantas on 1/16/19.
+//  Copyright © 2019 TMKang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount = 0
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+        flipCount += 1
+    }
+    
+    func flipCard (withEmoji emoji : String, on button: UIButton) {
+        print("flipCard withEmoji: \(emoji)")
+        if (button.currentTitle == "👻") {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else {
+            button.setTitle("👻", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+}
+
