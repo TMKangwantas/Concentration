7fcaaa4ad2cc06e5884fbcfd12bc69f3992cabcd
TMKangwantas <tmkangwantas@gmail.com>
2019-01-16 19:25:32 -0500

    Changed label to be included as flipCount's Property Observer. Also added 2 more cards and created an Outlet Collection for the cards.

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -10,26 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var flipCount = 0
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
         flipCount += 1
-    }
-    
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🎃", on: sender)
-        flipCount += 1
+        let cardNumber = cardButtons.index(of: sender)
+        print("cardNumber: \(cardNumber)")
     }
     
     func flipCard (withEmoji emoji : String, on button: UIButton) {
         print("flipCard withEmoji: \(emoji)")
-        if (button.currentTitle == "👻") {
+        if (button.currentTitle == emoji) {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         }
         else {
-            button.setTitle("👻", for: UIControl.State.normal)
+            button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
